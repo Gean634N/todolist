538a5a9f92b5fe8ceead5c8f8ade6e5a044bfe57
Gean634N <gean.a.l.c@gmail.com>
2021-10-14 07:17:03 -0400

    add checkbox to list itens

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import * as C from './styles';
 import { Item } from '../../types/item';
 type Props = {
@@ -5,9 +6,18 @@ type Props = {
 }
 
 export const ListItem = ({ item }: Props) => {
+  const [isChecked, setIsChecked] = useState(item.done);
+  
   return (
-    <C.Container>
-      { item.name }
+    <C.Container >
+      <input 
+        type="checkbox"
+        checked={ isChecked }
+        onChange={ e => setIsChecked(e.target.checked) }
+      />
+      <label>
+        { item.name }
+      </label>
     </C.Container>
   );
 }
--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: fles;
+  background-color: #20212C;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 5px 0;
+  align-items: center;
+
+  input {
+    width: 15px;
+    height: 15px;
+    margin-right: 5px;
+  }
+
+  label {
+    color: #eee;
+  }
+`;
